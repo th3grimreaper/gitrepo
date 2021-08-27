fbd7cc6b32746d34f3e6618cfed29d6defe79841
th3grimreaper <saip589@gmail.com>
2021-08-27 21:24:52 +0530

    added a program which demonstrates the use of try, throw and catch  for exception handling in C++

---
+++ C++/excephl.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+void err(int j)
+{
+    cout<<j<<" cannot be accepted as an argument in this context."<<'\n';
+}
+int square(int x)
+{
+    return x*x;
+}
+int main()
+{
+    int i;
+    cout<<"enter a number to know it's square: ";
+    cin>>i;
+    try {
+        if (i<0)
+            throw i;
+        cout<<"Square of "<<i<<" is : "<<square(i)<<'\n'; 
+    }
+    catch(int e) {
+        err(e);
+    }   
+    return 0;
+}
+
